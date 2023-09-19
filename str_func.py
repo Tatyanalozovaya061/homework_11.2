26a9aedf26fc7931f12a7f475280762446f1651c
tatyana lozovaya <tatka8632@gmail.com>
2023-09-19 23:06:00 +0300

    new func 123

--- str_func.py
+++ str_func.py
@@ -4,5 +4,6 @@ def uppercase_string(string):
 
 
 def uppercase_string_new(string):
-    """функция, которая переводит первые буквы в заглавные"""
+    """123функция, которая переводит первые буквы в заглавные"""
     return string.title()
+
